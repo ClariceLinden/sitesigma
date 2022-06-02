4586a4a77f4e75ec8435d51865fc52f6e9e4569f
Fah <103116456+vihh04@users.noreply.github.com>
2022-06-02 12:27:00 -0300

    att 1

---
+++ includes/_dados.php
@@ -0,0 +1,12 @@
+<?php
+
+$produtos = array (
+    0 => array('nome'=>'Capivara Legal','preco'=>'1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpeg'),
+    1 => array('nome'=>'Lontra Legal','preco'=>'1200.00','descricao'=>'Descrição Lontra Legal','imagem'=>'lontra-legal.jpeg'),
+    2 => array('nome'=>'=','preco'=>'','descricao'=>'Descrição','imagem'=>''),
+    3 => array('nome'=>'=','preco'=>'','descricao'=>'Descrição','imagem'=>''),
+    4 => array('nome'=>'=','preco'=>'','descricao'=>'Descrição','imagem'=>''),
+
+);
+
+?>

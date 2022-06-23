101c59396978048496a059543fd09c2e35bccef1
Clarice Seligman <79603269+ClariceLinden@users.noreply.github.com>
2022-06-23 12:24:05 -0300

    Add files via upload

---
+++ sitesigma/includes/_dados.php
@@ -0,0 +1,14 @@
+<?php
+
+$produtos = array (
+    0 => array('nome'=>'Capivara Legal','preco'=>'1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'),
+    1 => array('nome'=>'Lontra Legal','preco'=>'1200.00','descricao'=>'Descrição Lontra Legal','imagem'=>'lontra-legal.jpg'),
+    2 => array('nome'=>'Suricato Legal','preco'=>'1200.00','descricao'=>'Descrição Suricato Legal','imagem'=>'suricato-legal.jpg'),
+    3 => array('nome'=>'Gato Legal','preco'=>'1500.00','descricao'=>'Descrição Gato Legal','imagem'=>'gato-legal.jpg'),
+    4 => array('nome'=>'','preco'=>'','descricao'=>'Descrição','imagem'=>''),
+    5 => array('nome'=>'=','preco'=>'','descricao'=>'Descrição','imagem'=>''),
+    
+
+);
+
+?>
---
+++ sitesigma/index.php
@@ -0,0 +1,31 @@
+<?php
+// include do footer
+include_once './includes/_dados.php';
+include_once './includes/_head.php';
+include_once './includes/_header.php';
+?> 
+
+<div class="container">
+    <div class="row mt-5">
+    <?php
+    //laço de repetição para exibir os três primeiros produtos
+   for ($i=0; $i < 3 ; $i++) { 
+     ?>
+        <div class="card m-3" style="width: 18rem;">
+         <img src="./content/<?php echo $produtos[$i]['imagem'];?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $produtos[$i]['nome'];?></h5>
+                <p class="card-text"><?php echo $produtos[$i]['descricao'];?></p>
+                <a href="produto-detalhe.php?id=<?php echo $i;?>&tipo=promocao" class="btn btn-primary">Comprar</a>
+            </div>
+        </div>
+    <?php
+     }
+    ?>
+  </div>
+</div>
+
+<?php
+// include do footer
+include_once './includes/_footer.php';
+?>

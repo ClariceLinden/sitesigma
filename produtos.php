cd20f9dce9eba384e103e1c895dde9e798ee980e
ClariceLinden <79603269+ClariceLinden@users.noreply.github.com>
2022-06-30 10:33:34 -0300

    att 3

--- produtos.php
+++ produtos.php
@@ -5,7 +5,23 @@ include_once './includes/_head.php';
 include_once './includes/_header.php';
 ?>
 
-<h1>Produtos</h1>
+<?php
+foreach ($produtos as $key => $value) {
+    ?>
+
+    <div class="card m-3" style="width: 18rem;">
+         <img src="./content/<?php echo $value['imagem'];?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $value['nome'];?></h5>
+                <p class="card-text"><?php echo $value['descricao'];?></p>
+                <a href="produto-detalhe.php?id=<?php echo $key;?>&tipo=promocao" class="btn btn-primary">Comprar</a>
+            </div>
+        </div>
+<?php
+}
+?>
+
+
 
 <?php
 // include do footer
